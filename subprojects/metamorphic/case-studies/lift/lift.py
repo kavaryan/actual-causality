1a12a97de1fe23c96f7d49e21b60393ddc7ee1d6
“Kaveh <57283761+kavaryan@users.noreply.github.com>
2025-10-08 23:56:26 +0100

    feat: implement LiftSearchSpace class for lift simulation and goal checking

---
+++ subprojects/metamorphic/case-studies/lift/lift.py
@@ -0,0 +1,18 @@
+from subprojects.metamorphic.search_formulation import SearchSpace
+
+
+class LiftSearchSpace(SearchSpace):
+    def __init__(self, simulate_lifts_func, awt_thr):
+        self.simulate_lifts_func = simulate_lifts_func
+        self.awt_thr = awt_thr
+        super().__init__(list(range(len(simulate_lifts_func))))
+
+    def is_goal(self, X, v):
+        x = [v[i] for i in X]
+        rest = [v[i] for i in range(len(v)) if i not in X]
+        x_prime = [0 if x_i else 1 for x_i in x]
+        num_lifts = sum(rest) + sum(x_prime)
+        if num_lifts == 0:
+            return False
+        awt = self.simulate_lifts_func(num_lifts)
+        return awt <= self.awt_thr
