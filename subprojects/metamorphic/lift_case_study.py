4f9852e1a5f3311c8eee0c016ca99bfaac450231
“Kaveh <57283761+kavaryan@users.noreply.github.com>
2025-11-04 13:03:07 +0000

    feat: add non-interactive matplotlib backend to fix plotting issues

--- subprojects/metamorphic/lift_case_study.py
+++ subprojects/metamorphic/lift_case_study.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')  # Use non-interactive backend
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
